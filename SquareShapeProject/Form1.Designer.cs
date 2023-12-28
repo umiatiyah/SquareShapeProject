cb42197b1031f5b3e591b48e916a470dba9cabaf
umi.atiyah <umi.atiyah@axa-mandiri.co.id>
2023-12-28 23:47:41 +0700

    create windows form show picture box change color with API trigger by button and text input

--- SquareShapeProject/Form1.Designer.cs
+++ SquareShapeProject/Form1.Designer.cs
@@ -29,13 +29,59 @@ namespace SquareShapeProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(269, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(233, 153);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.pictureBox1_LoadCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Change Color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(312, 251);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- SquareShapeProject/Form1.cs
+++ SquareShapeProject/Form1.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,10 +16,55 @@ namespace SquareShapeProject
 {
     public partial class Form1 : Form
     {
+        private Color squareShapeColor = Color.Green;
         public Form1()
         {
             InitializeComponent();
+            DrawSquareShape();
         }
 
+        private void DrawSquareShape()
+        {
+            this.pictureBox1.BackColor = squareShapeColor;         
+        }
+
+        private void RecolorSquareShape(string color)
+        {
+            Color newColor = ColorTranslator.FromHtml(color);
+            squareShapeColor = newColor;
+            BeginInvoke(new MethodInvoker(() =>
+            {
+                DrawSquareShape();
+            }));
+        }
+
+        private void StartHttpServer(string url, string newColor)
+        {
+            Thread thread = new Thread(async () =>
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    var jsonObject = JsonConvert.SerializeObject(newColor);
+                    var content = new StringContent(jsonObject, Encoding.UTF8, "application/json");
+                    HttpResponseMessage httpResponse = await httpClient.PostAsync(url, content);
+                    if (httpResponse.IsSuccessStatusCode)
+                    {
+                        RecolorSquareShape(newColor);
+                    }
+                }
+            });
+            thread.Start();
+        }
+
+        private void pictureBox1_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            this.BackColor = squareShapeColor;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "https://localhost:44343/Color/changeColor/";
+            StartHttpServer(url, this.textBox1.Text);
+        }
     }
 }
